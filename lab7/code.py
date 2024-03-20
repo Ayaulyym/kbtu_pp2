00a96158ad2c9c5495fb4ae9dd8cd07af59d51f3
Ayaulyym <dusaaulym@gmail.com>
2024-03-20 22:42:37 +0600

    clock

---
+++ lab7/code.py
@@ -0,0 +1,41 @@
+import pygame 
+from datetime import datetime 
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,800))
+window_title = pygame.display.set_caption("Clock")
+
+clock = pygame.time.Clock()
+
+
+bg_surf = pygame.image.load(r"IMAGE/clock.png")
+sec_surf = pygame.image.load(r"IMAGE/sec.png")
+min_surf = pygame.image.load(r"IMAGE/min.png")
+bg_rect = bg_surf.get_rect(center = (400, 400))
+
+done = False
+
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    now=datetime.now().time()
+
+    second_angle = -((now.second+1) * 6)
+    minute_angle = -((now.minute+9) * 6) 
+
+    rotated_sec = pygame.transform.rotate(sec_surf, second_angle)
+    rotated_min = pygame.transform.rotate(min_surf, minute_angle)
+
+    sec_rect = rotated_sec.get_rect(center = bg_rect.center)
+    min_rect = rotated_min.get_rect(center = bg_rect.center)
+
+    screen.blit(bg_surf, bg_rect)
+    screen.blit(rotated_sec, sec_rect)
+    screen.blit(rotated_min, min_rect)
+
+    pygame.display.flip()
+    clock.tick(60)
